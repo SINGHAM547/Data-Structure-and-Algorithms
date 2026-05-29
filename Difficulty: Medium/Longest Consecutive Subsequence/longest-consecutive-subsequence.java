209c0d051a1795667877ac16b9191eef37569fef
Sumeet Sahu <121349447+SINGHAM547@users.noreply.github.com>
2026-05-29 15:21:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestConsecutive(int[] arr) {
+
+        if (arr.length == 0) {
+            return 0;
+        }
+        Arrays.sort(arr);
+        int count = 1;
+        int ans = 1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] + 1 == arr[i + 1]) {
+                count++;
+            }
+            else if (arr[i] == arr[i + 1]) {
+                continue;
+            }
+            else {
+                count = 1;
+            }
+            ans = Math.max(ans, count);
+        }
+        return ans;
+    }
+}
