572d5941c5699c26d4c8ca772a620164678d0f5e
Sumeet Sahu <121349447+SINGHAM547@users.noreply.github.com>
2026-06-29 15:12:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
@@ -1,12 +1,16 @@
 class Solution {
     public ArrayList<Integer> mergeArrays(int[][] mat) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                ans.add(mat[i][j]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                pq.offer(mat[i][j]);
             }
         }
-        Collections.sort(ans);
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll());
+        }
         return ans;
     }
 }
