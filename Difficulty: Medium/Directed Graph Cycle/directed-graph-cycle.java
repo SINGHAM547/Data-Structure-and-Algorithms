2f5668b170635417bc1ec5ffd983b57b81ba5b96
Sumeet Sahu <121349447+SINGHAM547@users.noreply.github.com>
2026-07-08 01:03:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,35 +1,34 @@
 class Solution {
-    boolean dfs(int u,ArrayList<ArrayList<Integer>> adj,boolean[] visited, boolean[] recStack){
-    
-        visited[u]=true;
-        recStack[u]=true;
-        
-        for(int v:adj.get(u)){
-            if(!visited[v]){
-                if(dfs(v,adj,visited,recStack))return true;
-            }else if(recStack[v]){
-                return true;
-            }
-        }
-        recStack[u]=false;
-        return false;
-    }
     public boolean isCyclic(int V, int[][] edges) {
         ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
         }
-        for(int [] edge:edges){
-            adj.get(edge[0]).add(edge[1]);
-        }
-        boolean[] visited=new boolean[V];
-        boolean[] recStack= new boolean[V];
+        int [] indegree= new int[V];
         
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
+            indegree[v]++;
+        }
+        Queue<Integer> q= new LinkedList<>();
         for(int i=0;i<V;i++){
-            if(!visited[i]){
-                if(dfs(i,adj,visited,recStack)) return true;
+            if(indegree[i]==0){
+                q.offer(i);
             }
         }
-        return false;
+        int count=0;
+        while(!q.isEmpty()){
+            int u=q.poll();
+            count++;
+            for(int v:adj.get(u)){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.offer(v);
+                }
+            }
+        }
+        return count!=V;
     }
 }
