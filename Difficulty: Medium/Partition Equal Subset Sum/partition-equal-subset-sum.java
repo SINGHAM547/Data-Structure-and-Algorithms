cc0906f1e5e6a77e919bbe947f186b6fe949a73f
Sumeet Sahu <121349447+SINGHAM547@users.noreply.github.com>
2026-07-25 19:48:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    static boolean equalPartition(int arr[]) {
+        int sum=0;
+        for(int x:arr){
+            sum+=x;
+        }
+        if(sum%2!=0)return false;
+        int target=sum/2;
+        int n =arr.length;
+        
+        boolean[][] dp = new boolean[n+1][target+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j] = dp[i-1][j]|| dp[i-1][j-arr[i-1]];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+}
